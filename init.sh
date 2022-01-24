868c35caf1fe805abf73087a4b7f7be1204da6ba
Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>
2022-01-24 12:39:57 +0100

    Small patches to make irods+sssd work.

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-mkdir -p volumes/{postgres/data,redis/data,traefik/letsencrypt,irods/{log,vault}}
+mkdir -p volumes/{postgres/data,redis/data,traefik/letsencrypt,irods/{log,vault}} config/irods/etc config/davrods/theme
+chown 1000:1000 volumes/irods/*
