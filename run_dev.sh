f3b6d1b42af63a8cc1e48a6b01a4bec9765c11a7
Dzmitry Hramyka <grom.dima.grom@gmail.com>
2023-09-29 09:01:18 +0200

    update docker compose command in scripts (#45)

--- run_dev.sh
+++ run_dev.sh
@@ -2,7 +2,7 @@
 # Development helper for running external components ONLY.
 # SODAR server components are expected to be run locally on the host.
 # You can also use the .sssd override if you need to develop with LDAP.
-docker-compose -f docker-compose.dev.yml \
+docker compose -f docker-compose.dev.yml \
     -f docker-compose.override.yml.provided-cert \
     up \
     --remove-orphans
